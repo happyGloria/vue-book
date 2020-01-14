9fd450faea5767d0e026dee1854d75dfd63ae215
zhaodan <zhaodan@netposa.com>
2020-01-14 17:55:41 +0800

    font-family

---
+++ src/components/ebook/EbookSettingFontPopup.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="ebook-popup-list">
+    <div class="ebook-popup-title">
+      <div class="ebook-popup-title-icon" @click="hideFontFamilySetting">
+        <span class="icon icon-down2"></span>
+      </div>
+      <span class="ebook-popup-title-text"></span>
+    </div>
+  </div>
+</template>
+<script>
+import { EbookMixin } from './mixin.js'
+export default {
+  mixins: [EbookMixin],
+  methods: {
+    hideFontFamilySetting () {
+
+    }
+  }
+}
+</script>
+<style lang="scss" rel="stylesheet/scss" scoped></style>
